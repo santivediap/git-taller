dec2a6fe462964b7042ddcba088791a217858601
SVega9848 <santivediap@gmail.com>
2023-05-05 15:56:04 +0200

    add/submit_event

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const btn = document.querySelector("#btn");
 btn.addEventListener("click", function() {
     document.body.setAttribute("background-color", "pink");
 })
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const userName = event.target.name.value;
+    const userPassword = event.target.password.value;
+
+    if(userName == "") {
+        console.log("Nombre requerido");
+    }
+
+    if(userPassword == "") {
+        console.log("Contraseña requerida");
+    }
+})
